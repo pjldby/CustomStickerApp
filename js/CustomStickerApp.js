720e93632fe6d59bcb97f0f8e2e8372be939327e
pranjal dubey <pjldby@gmail.com>
2015-07-17 23:28:52 +0530

    moved fontsize increasing code to this CustomStickerApp.js

--- js/CustomStickerApp.js
+++ js/CustomStickerApp.js
@@ -1,3 +1,11 @@
+/* NORMAL JAVASCRIPT CODE */
+function changeFontSize(vol)
+{
+  vol=vol+'px';
+  sticker.style.fontSize=vol;
+}
+
+/* AMGULAR JS CODE */
 var CustomStickerApp = angular.module('CustomStickerApp',[]);
 
 CustomStickerApp.controller('CustomStickerController', function($scope){
